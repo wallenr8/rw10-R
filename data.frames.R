1ce109852057be7ff9d7e7c6a2589796ba93984a
wallenr8 <143291336+wallenr8@users.noreply.github.com>
2023-09-06 16:08:58 -0400

    Create data.frames.R

---
+++ data.frames.R
@@ -0,0 +1,17 @@
+> Names <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+
+> ABC <- c(4, 62, 51, 21, 2, 14, 15)
+> CBS <- c(12, 75, 43, 19, 1, 21, 19) 
+> df <- data.frame(Names, ABC, CBS)
+> df
+    Names ABC CBS
+1     Jeb   4  12
+2  Donald  62  75
+3     Ted  51  43
+4   Marco  21  19
+5   Carly   2   1
+6 Hillary  14  21
+7  Berine  15  19
+
+
+😊https://februarycyberdays.blogspot.com/2023/09/dataframes-in-r.html
